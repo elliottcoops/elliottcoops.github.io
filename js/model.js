63c612d51c55fa5a35f3724b42ef2afd1b9ce185
Elliott Cooper <psyec3@exmail.nottingham.ac.uk>
2024-06-03 21:07:54 +0100

    [Fix] - Change path to model

--- js/model.js
+++ js/model.js
@@ -1,5 +1,5 @@
 async function testModel() {
-    // const model = await tf.loadLayersModel('https://elliottcoops.github.io/model_js/model.json');
+    const model = await tf.loadLayersModel('../model_js/model.json');
     console.log('success');
 }
 
