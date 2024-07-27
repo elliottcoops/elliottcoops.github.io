0eee73499dba86993dea171d27efa4cb68d16c5d
Elliott Cooper <elliottcoops@gmail.com>
2024-07-27 19:10:13 +0100

    <Chore>: Add new chars to the array

--- js/Character-Recognition/draw.js
+++ js/Character-Recognition/draw.js
@@ -24,6 +24,11 @@ for (let i = 65; i < 91; i++) {
     characters.push(String.fromCharCode(i));
 }
 
+
+for (let i = 97; i < 122; i++){
+    characters.push(String.fromCharCode(i));
+}
+
 // Get the scaled coordinates 
 function getScaledCoordinates(event) {
     const rect = canvas.getBoundingClientRect();
