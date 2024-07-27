21b2343bc57eff658018966878f77ef6074d8415
Elliott Cooper <elliottcoops@gmail.com>
2024-07-27 22:24:52 +0100

    <Fix>: Add old model back

--- js/Character-Recognition/model.js
+++ js/Character-Recognition/model.js
@@ -2,7 +2,7 @@ window.myModel = null;
 
 async function testModel() {
     try {
-        myModel = await tf.loadLayersModel('../model_js/model.json');
+        myModel = await tf.loadLayersModel('https://raw.githubusercontent.com/elliottcoops/elliottcoops.github.io/main/model_js/model.json');
         console.log('Model loaded successfully');
     } catch (error) {
         console.error('Error loading the model:', error);
