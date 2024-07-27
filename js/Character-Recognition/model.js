591c46ceaffb952500ca0113ef1bc9d8d6bc003f
Elliott Cooper <elliottcoops@gmail.com>
2024-07-27 19:35:36 +0100

    <Chore>: Add error handling to model loading

--- js/Character-Recognition/model.js
+++ js/Character-Recognition/model.js
@@ -1,7 +1,12 @@
 window.myModel = null;
 
 async function testModel() {
-    window.myModel = await tf.loadLayersModel('../model_js/model.json');
+    try {
+        myModel = await tf.loadLayersModel('model_js/model.json');
+        console.log('Model loaded successfully');
+    } catch (error) {
+        console.error('Error loading the model:', error);
+    }
 }
 
 testModel();
